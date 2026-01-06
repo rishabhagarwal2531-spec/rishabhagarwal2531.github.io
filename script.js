a8027f998645363aa156c37a986f7f4a381a20e5
rishabhagarwal2531-spec <rishabhagarwal2531@gmail.com>
2026-01-06 23:09:57 +0530

    Add right-click event handlers and showMessage function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.addEventListener("contextmenu", function(event) {
   event.preventDefault();
   alert("Right-click detected!");
 });
+// Show Message button function
+function showMessage() {
+    alert("This is a test message for Selenium practice!");
+}
+
+// Right-click context menu
+const box = document.getElementById("rightClickBox");
+
+box.addEventListener("contextmenu", function (event) {
+    event.preventDefault(); // stop browser default menu
+    alert("Right-click action detected!");
+});
+
